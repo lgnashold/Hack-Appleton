0cca4d9c0eb1ae9188882240f24f54553121d2f6
Ironbizmark <anna.arpacidusseau@gmail.com>
2018-04-08 11:57:25 -0500

    organized java

--- Website/readJSON.js
+++ Website/readJSON.js
@@ -1,5 +1,17 @@
-var JSON;
-function getDataset (name) {
-	var dataset = JSON.name;
-	return dataset;
+var JSONobject = JSON.parse('{ "continent": {"europe":[ {"x":10, "y":11}, {"x":10} ], "africa":20} , "age":30, "city":"New York"}');
+
+
+function getDataset (minorCat) {
+	return JSONobject[minorCat];
 }
+
+function getMinorCategorys(majorCat) {
+	var minorCats = [];
+	var label;
+	for(label in JSONobject[majorCat]) {
+		minorCats.push(label);
+	}
+	return minorCats;
+}
+
+console.log(getMinorCategorys("continent"));
