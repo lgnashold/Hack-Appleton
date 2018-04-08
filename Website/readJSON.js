089b63be9a21aee67d706eb13645f1cb899f8bc2
Ironbizmark <anna.arpacidusseau@gmail.com>
2018-04-08 13:21:52 -0500

    trying to fix things

--- Website/readJSON.js
+++ Website/readJSON.js
@@ -1,15 +1,29 @@
-var JSONobject = JSON.parse('{"Continent":{"Europe":[{"x":1.0,"y":5.0}],"Africa":[],"SouthAmerica":[],"NorthAmerica":[],"Antarctica":[],"Asia":[{"x":3.0,"y":6.0}],"Australia":[]},"Age":{"EighteenToThirty":[{"x":3.0,"y":6.0}],"ThirtyToFifty":[],"ThirteenToEighteen":[],"UnderThirteen":[{"x":1.0,"y":5.0}],"FiftyAndOlder":[]},"Gender":{"Male":[],"Female":[{"x":1.0,"y":5.0}],"Other":[{"x":3.0,"y":6.0}]}}');
-function getDataset (majorCat, minorCat) {
-	return {label: minorCat, data: JSONobject[majorCat][minorCat]};
+var JSONobject = JSON.parse('{"Gender":{"Male":[],"Other":[{"x":14.0,"y":3.0},{"x":15.0,"y":3.0},{"x":16.0,"y":8.0},{"x":17.0,"y":3.0}],"Female":[{"x":14.0,"y":4.0},{"x":15.0,"y":9.0},{"x":16.0,"y":10.0},{"x":17.0,"y":10.0}]},"Age":{"ThirteenToEighteen":[{"x":15.0,"y":1.0},{"x":16.0,"y":5.0},{"x":17.0,"y":1.0}],"EighteenToThirty":[{"x":14.0,"y":1.0},{"x":15.0,"y":3.0},{"x":16.0,"y":2.0},{"x":17.0,"y":6.0}],"ThirtyToFifty":[{"x":14.0,"y":1.0},{"x":15.0,"y":1.0},{"x":16.0,"y":4.0},{"x":17.0,"y":1.0}],"FiftyAndOlder":[{"x":14.0,"y":5.0},{"x":15.0,"y":6.0},{"x":16.0,"y":3.0},{"x":17.0,"y":4.0}],"UnderThirteen":[{"x":15.0,"y":1.0},{"x":16.0,"y":4.0},{"x":17.0,"y":1.0}]},"Continent":{"NorthAmerica":[{"x":14.0,"y":2.0},{"x":15.0,"y":2.0},{"x":17.0,"y":1.0}],"Africa":[{"x":14.0,"y":1.0},{"x":15.0,"y":2.0},{"x":16.0,"y":3.0},{"x":17.0,"y":3.0}],"Asia":[{"x":14.0,"y":1.0},{"x":16.0,"y":4.0},{"x":17.0,"y":1.0}],"Europe":[{"x":14.0,"y":1.0},{"x":15.0,"y":1.0},{"x":16.0,"y":2.0},{"x":17.0,"y":1.0}],"SouthAmerica":[{"x":14.0,"y":2.0},{"x":15.0,"y":5.0},{"x":16.0,"y":2.0},{"x":17.0,"y":5.0}],"Australia":[{"x":15.0,"y":1.0},{"x":16.0,"y":4.0}],"Antarctica":[{"x":15.0,"y":1.0},{"x":16.0,"y":3.0},{"x":17.0,"y":2.0}]}}');
+function getDataset (majorCat, minorCat, color) {
+	return {
+		label: minorCat, data: JSONobject[majorCat][minorCat],
+		fill:false,borderWidth:5,borderColor:color
+	};
 }
 
-function getData(majorCat) {
+function getAllDatasets(majorCat, colorList) {
 	var datasets = [];
 	var label;
+	var count = 0;
 	for(label in JSONobject[majorCat]) {
-		datasets.push(getDataset(majorCat,label));
+		datasets.push(getDataset(majorCat,label,colorList[count]));
+		count++;
 	}
 	return datasets;
+}
+
+function getMajorCategorys() {
+	var majorCats = [];
+	var label;
+	for(label in JSONobject) {
+		majorCats.push(label);
+	}
+	return majorCats;
 }
 
 function getMinorCategorys(majorCat) {
@@ -21,5 +35,5 @@ function getMinorCategorys(majorCat) {
 	return minorCats;
 }
 
-console.log(getData("Continent"));
+console.log(getMajorCategorys());
 
